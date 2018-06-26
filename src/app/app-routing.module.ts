b54adfc36be02b6e6f4aaefada4becb76d873c45
Carl Dawson <carl@invisible-gorilla.com>
2018-06-26 07:50:57 -0400

    Added quote module and quote component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "@app/core/home/home.component";
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+	{ path: 'quote/:id', loadChildren: './quote/quote.module#QuoteModule' }
 ];
 
 @NgModule({
