7949a01dfc8e09fcccb93cfc95278b1967251134
Carl Dawson <carl@invisible-gorilla.com>
2018-06-30 07:39:41 -0400

    Updated quote model, updated admin component html/css

--- src/app/models/quote.ts
+++ src/app/models/quote.ts
@@ -2,6 +2,9 @@ export class Quote {
   constructor(
     public email: string,
     public description: string,
+    public createdAt?: string,
+    public updatedAt?: string,
+    public status?: string,
     public _id?: string,
     public __v?: number
   ) {}
