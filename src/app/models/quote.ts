1231369f0b6c8cfcf357eb624d620666a313ead1
Carl Dawson <carl@invisible-gorilla.com>
2018-07-02 12:00:11 -0400

    Added comment to quote model

--- src/app/models/quote.ts
+++ src/app/models/quote.ts
@@ -2,9 +2,21 @@ export class Quote {
   constructor(
     public email: string,
     public description: string,
+    public comments?: Comment[],
     public createdAt?: string,
     public updatedAt?: string,
     public status?: string,
+    public _id?: string,
+    public __v?: number
+  ) {}
+}
+
+export class Comment {
+  constructor(
+    public message: string,
+    public admin: boolean,
+    public createdAt?: string,
+    public updatedAt?: string,
     public _id?: string,
     public __v?: number
   ) {}
