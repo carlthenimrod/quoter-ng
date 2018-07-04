11de70b20a8901dc8ecec232fe0e46b6564a80c8
Carl Dawson <carl@invisible-gorilla.com>
2018-07-04 16:46:51 -0400

    admin quote html/css edits

--- src/app/admin/admin-quote/admin-quote.component.ts
+++ src/app/admin/admin-quote/admin-quote.component.ts
@@ -14,6 +14,9 @@ export class AdminQuoteComponent implements OnInit {
 
   quote: Quote;
   comment: Comment;
+  latestUserComment: Comment;
+  latestAdminComment: Comment;
+  latestComment: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,6 +27,11 @@ export class AdminQuoteComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.quoteService.get(params['id']).subscribe((quote: Quote) => {
+
+        //remove latest comments
+        quote.comments = this.removeLatestComments(quote.comments);
+
+        //store quote
         this.quote = quote;
 
         //create new comment, set admin flag to true
@@ -35,10 +43,46 @@ export class AdminQuoteComponent implements OnInit {
     });
   }
 
+  removeLatestComments(comments: Comment[]): Comment[] {
+
+    let foundUserComment: boolean = false;
+    let foundAdminComment: boolean = false;
+    let filteredComments: Comment[];
+
+    filteredComments = comments.filter(comment => {
+
+      //only get admin comment if no user comment is present
+      if (comment.admin && !foundAdminComment && !foundUserComment) {
+        this.latestAdminComment = comment;
+        foundAdminComment = true;
+      } else if (!foundUserComment && !comment.admin) {
+        this.latestUserComment = comment;
+        foundUserComment = true;
+      } else return comment;
+    });
+
+    //find who made the latest comment
+    if (this.latestAdminComment && this.latestUserComment) {
+      const adminCommentDate = new Date(this.latestAdminComment.updatedAt);
+      const userCommentDate = new Date(this.latestUserComment.updatedAt);
+
+      this.latestComment = (adminCommentDate > userCommentDate) ? 'admin' : 'user';
+    }
+    else if (this.latestAdminComment) this.latestComment = 'admin';
+    else if (this.latestUserComment) this.latestComment = 'user';
+
+    return filteredComments;
+  }
+
   onSubmit() {
 
     //add comment
     this.commentService.add(this.quote._id, this.comment).subscribe((quote: Quote) => {
+
+      //remove latest comments
+      quote.comments = this.removeLatestComments(quote.comments);
+
+      //store quote
       this.quote = quote;
     },
     e => {
