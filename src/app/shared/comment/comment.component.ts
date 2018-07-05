55e6a27f0098a813220880fd0d208ce39a1e6b45
Carl Dawson <carl@invisible-gorilla.com>
2018-07-05 13:38:23 -0400

    Added options for comment component, edit/delete buttons

--- src/app/shared/comment/comment.component.ts
+++ src/app/shared/comment/comment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Comment } from '@app/models/quote';
 
@@ -7,8 +7,26 @@ import { Comment } from '@app/models/quote';
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.scss']
 })
-export class CommentComponent {
+export class CommentComponent implements OnInit {
 
   @Input() comment: Comment;
   @Input() me: boolean;
+  @Input() size: string;
+  @Input() editable: boolean = false;
+  @Input() removable: boolean = false;
+
+  ngOnInit() {
+    switch (this.size) {
+      case 'sm':
+        this.size = '1rem';
+        break;
+
+      case 'lg':
+        this.size = '1.4rem';
+        break;
+
+      default:
+        this.size = '1.2rem'
+    }
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CommentComponent } from './comment/comment.component';
+import { CommentFormComponent } from './comment-form/comment-form.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [
-    CommentComponent
+    CommentComponent,
+    CommentFormComponent
   ],
   declarations: [
-    CommentComponent
+    CommentComponent,
+    CommentFormComponent
   ]
 })
 export class SharedModule { }
